46409974e1bc938a45efc1a5eebb1b393b39f2f7
hs434 <hs434@njit.edu>
2019-12-01 18:53:04 -0500

    Task: Created an insert file

---
+++ Database/inserts.py
@@ -0,0 +1,44 @@
+# 1 - imports
+from base import Session, engine, Base
+from sqlalchemy.orm import  sessionmaker, Session
+
+from base import Customer
+from pprint import pprint
+
+
+
+# 2 - generate database schema
+Base.metadata.create_all(engine)
+Base.metadata.bind = engine
+
+# 3 - create a new session
+session = Session()
+
+# 4 - create Customer
+c1 = Customer(first_name='Toby',
+              last_name='Miller',
+              username='tmiller',
+              email='tmiller@example.com',
+              address='1662 Kinney Street',
+              town='Wolfden'
+
+              )
+
+c2 = Customer(first_name='Scott',
+              last_name='Harvey',
+              username='scottharvey',
+              email='scottharvey@example.com',
+              address='424 Patterson Street',
+              town='Beckinsdale'
+
+              )
+
+
+print(c1.first_name , c1.last_name)
+print(c2.first_name , c2.last_name)
+session.add(c1)
+session.add(c2)
+pprint(engine.table_names())
+# 10 - commit and close session
+session.commit()
+session.close()
